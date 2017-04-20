980329fb8cc96dbc097f20a722978e35bda81a48
V-ed <guillaumemarcoux@gmail.com>
2017-04-20 15:45:14 -0400

    Changed the ScreenVueArtiste to JDialog to make it modal-able.

--- GuillaumeMarcoux_WilliamMalenfant_TP2/src/graphics/ScreenVueArtistes.java
+++ GuillaumeMarcoux_WilliamMalenfant_TP2/src/graphics/ScreenVueArtistes.java
@@ -9,7 +9,7 @@ import java.awt.GridBagConstraints;
 import java.awt.Insets;
 import java.awt.GridLayout;
 
-public class ScreenVueArtistes extends JFrame {
+public class ScreenVueArtistes extends JDialog {
 	
 	private JLabel labelNoArtisteSelected = new JLabel(
 			"Veuillez s\u00E9lectionner un artiste!");
@@ -32,7 +32,9 @@ public class ScreenVueArtistes extends JFrame {
 	
 	private MySQLDatabase database;
 	
-	public ScreenVueArtistes(MySQLDatabase database){
+	public ScreenVueArtistes(MySQLDatabase database, JFrame parentFrame){
+		
+		super(parentFrame, true);
 		
 		this.database = database;
 		
@@ -53,7 +55,7 @@ public class ScreenVueArtistes extends JFrame {
 		{
 			1.0, 1.0, Double.MIN_VALUE
 		};
-		getContentPane().setLayout(gridBagLayout);
+		setLayout(gridBagLayout);
 		
 		GridBagConstraints gbc_panelOperations = new GridBagConstraints();
 		gbc_panelOperations.gridheight = 2;
@@ -61,19 +63,19 @@ public class ScreenVueArtistes extends JFrame {
 		gbc_panelOperations.fill = GridBagConstraints.BOTH;
 		gbc_panelOperations.gridx = 0;
 		gbc_panelOperations.gridy = 0;
-		getContentPane().add(createOperationsPanel(), gbc_panelOperations);
+		add(createOperationsPanel(), gbc_panelOperations);
 		
 		GridBagConstraints gbc_panelVueArtiste = new GridBagConstraints();
 		gbc_panelVueArtiste.fill = GridBagConstraints.BOTH;
 		gbc_panelVueArtiste.gridx = 1;
 		gbc_panelVueArtiste.gridy = 0;
-		getContentPane().add(createArtisteView(), gbc_panelVueArtiste);
+		add(createArtisteView(), gbc_panelVueArtiste);
 		
 		GridBagConstraints gbc_panelChoixArtiste = new GridBagConstraints();
 		gbc_panelChoixArtiste.fill = GridBagConstraints.BOTH;
 		gbc_panelChoixArtiste.gridx = 1;
 		gbc_panelChoixArtiste.gridy = 1;
-		getContentPane().add(createChoixArtisteView(), gbc_panelChoixArtiste);
+		add(createChoixArtisteView(), gbc_panelChoixArtiste);
 		
 		labelNoArtisteSelected.setHorizontalAlignment(SwingConstants.CENTER);
 		labelNoArtisteSelected.setBounds(357, 70, 180, 29);
@@ -101,6 +103,8 @@ public class ScreenVueArtistes extends JFrame {
 		GridBagLayout gbl_panelAffichageArtiste = new GridBagLayout();
 		panelAffichageArtiste.setLayout(gbl_panelAffichageArtiste);
 		GridBagConstraints gbc_label_5 = new GridBagConstraints();
+		gbc_label_5.weighty = 1.0;
+		gbc_label_5.weightx = 1.0;
 		gbc_label_5.gridwidth = 2;
 		gbc_label_5.gridheight = 3;
 		gbc_label_5.insets = new Insets(0, 0, 5, 0);
@@ -108,36 +112,50 @@ public class ScreenVueArtistes extends JFrame {
 		gbc_label_5.gridy = 0;
 		panelAffichageArtiste.add(label_5, gbc_label_5);
 		GridBagConstraints gbc_label = new GridBagConstraints();
+		gbc_label.weighty = 1.0;
+		gbc_label.weightx = 1.0;
 		gbc_label.insets = new Insets(0, 0, 5, 5);
 		gbc_label.gridx = 0;
 		gbc_label.gridy = 1;
 		panelAffichageArtiste.add(label, gbc_label);
 		GridBagConstraints gbc_label_1 = new GridBagConstraints();
+		gbc_label_1.weighty = 1.0;
+		gbc_label_1.weightx = 1.0;
 		gbc_label_1.insets = new Insets(0, 0, 5, 5);
 		gbc_label_1.gridx = 1;
 		gbc_label_1.gridy = 1;
 		panelAffichageArtiste.add(label_1, gbc_label_1);
 		GridBagConstraints gbc_label_2 = new GridBagConstraints();
+		gbc_label_2.weighty = 1.0;
+		gbc_label_2.weightx = 1.0;
 		gbc_label_2.insets = new Insets(0, 0, 5, 5);
 		gbc_label_2.gridx = 0;
 		gbc_label_2.gridy = 3;
 		panelAffichageArtiste.add(label_2, gbc_label_2);
 		GridBagConstraints gbc_label_3 = new GridBagConstraints();
+		gbc_label_3.weighty = 1.0;
+		gbc_label_3.weightx = 1.0;
 		gbc_label_3.insets = new Insets(0, 0, 5, 5);
 		gbc_label_3.gridx = 1;
 		gbc_label_3.gridy = 3;
 		panelAffichageArtiste.add(label_3, gbc_label_3);
 		GridBagConstraints gbc_label_4 = new GridBagConstraints();
+		gbc_label_4.weighty = 1.0;
+		gbc_label_4.weightx = 1.0;
 		gbc_label_4.insets = new Insets(0, 0, 0, 5);
 		gbc_label_4.gridx = 0;
 		gbc_label_4.gridy = 5;
 		panelAffichageArtiste.add(label_4, gbc_label_4);
 		GridBagConstraints gbc_checkBox = new GridBagConstraints();
+		gbc_checkBox.weighty = 1.0;
+		gbc_checkBox.weightx = 1.0;
 		gbc_checkBox.insets = new Insets(0, 0, 0, 5);
 		gbc_checkBox.gridx = 1;
 		gbc_checkBox.gridy = 5;
 		panelAffichageArtiste.add(checkBox, gbc_checkBox);
 		GridBagConstraints gbc_button = new GridBagConstraints();
+		gbc_button.weighty = 2.0;
+		gbc_button.weightx = 1.0;
 		gbc_button.gridx = 3;
 		gbc_button.gridy = 5;
 		panelAffichageArtiste.add(button, gbc_button);
@@ -149,8 +167,6 @@ public class ScreenVueArtistes extends JFrame {
 	private JPanel createChoixArtisteView(){
 		
 		JPanel panelChoixArtiste = new JPanel();
-		
-		
 		
 		return panelChoixArtiste;
 		
