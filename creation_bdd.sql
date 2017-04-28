a2fa101e5d3619b0052e0672bb29b89341099f60
V-ed <guillaumemarcoux@gmail.com>
2017-04-27 20:54:46 -0400

    Added ON DELETE CASCADE to the database creation script.

--- creation_bdd.sql
+++ creation_bdd.sql
@@ -4,20 +4,20 @@ USE gestionalbums;
 
 CREATE TABLE artiste (
 	idArtiste int AUTO_INCREMENT PRIMARY KEY,
-    nom VARCHAR(50) NOT NULL,
-    prenom VARCHAR(50) NOT NULL,
-    estMembre BOOLEAN NOT NULL,
+	nom VARCHAR(50) NOT NULL,
+	prenom VARCHAR(50) NOT NULL,
+	estMembre BOOLEAN NOT NULL,
 	image VARCHAR(30)
 );
 
 CREATE TABLE album (
 	idAlbum int AUTO_INCREMENT PRIMARY KEY,
-    titre VARCHAR(40) NOT NULL,
-    prix DECIMAL(5,2) NOT NULL,
-    genre VARCHAR(12) NOT NULL,
-    anneeSortie DATE NOT NULL,
-    maisonDistribution VARCHAR(40) NOT NULL,
-    image VARCHAR(30),
-    idArtiste int,
-    FOREIGN KEY (idArtiste) REFERENCES artiste(idArtiste)
+	titre VARCHAR(40) NOT NULL,
+	prix DECIMAL(5,2) NOT NULL,
+	genre VARCHAR(12) NOT NULL,
+	anneeSortie DATE NOT NULL,
+	maisonDistribution VARCHAR(40) NOT NULL,
+	image VARCHAR(30),
+	idArtiste int,
+	FOREIGN KEY (idArtiste) REFERENCES artiste(idArtiste) ON DELETE CASCADE
 );
