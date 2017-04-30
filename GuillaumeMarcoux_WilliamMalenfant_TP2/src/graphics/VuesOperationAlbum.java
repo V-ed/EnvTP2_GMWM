e0f823acb506f82e09eb2f61c841005cb3e19ff2
V-ed <guillaumemarcoux@gmail.com>
2017-04-30 14:12:00 -0400

    Images are now working with VuesOperationAlbum

--- GuillaumeMarcoux_WilliamMalenfant_TP2/src/graphics/VuesOperationAlbum.java
+++ GuillaumeMarcoux_WilliamMalenfant_TP2/src/graphics/VuesOperationAlbum.java
@@ -17,7 +17,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
 import java.sql.Date;
-import java.text.NumberFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
 
@@ -31,8 +30,9 @@ import objects.DatePicker;
 import objects.MySQLDatabase;
 import objects.TableObject;
 
-public class VuesOperationAlbum extends JDialog implements Constantes, ConstantesAffichage {
-
+public class VuesOperationAlbum extends JDialog implements Constantes,
+		ConstantesAffichage {
+	
 	private JTextField textTitre;
 	private JTextField textPrix;
 	private JTextField textGenre;
@@ -41,29 +41,42 @@ public class VuesOperationAlbum extends JDialog implements Constantes, Constante
 	private JComboBox<Artiste> comboBox;
 	private JLabel lblPath;
 	private ArrayList<TableObject> listArtistes;
-
+	
 	private boolean hasConfirmed = false;
-
+	
 	private String[] columnNames;
 	private Object[] values;
-
-	public VuesOperationAlbum(MySQLDatabase database, VueAlbums vueAlbum, int typeOperation, Album album) {
-
+	
+	public VuesOperationAlbum(MySQLDatabase database, VueAlbums vueAlbum,
+			int typeOperation, Album album){
+		
 		super(vueAlbum, true);
-
+		
 		listArtistes = Artiste.getAllAsArrayList(database);
-
+		
 		setSize(530, 532);
 		setLocationRelativeTo(vueAlbum);
 		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
-
+		
 		GridBagLayout gridBagLayout = new GridBagLayout();
-		gridBagLayout.columnWidths = new int[] { 25, 73, 200, 37, 0 };
-		gridBagLayout.rowHeights = new int[] { 10, 57, 64, 53, 52, 55, 56, 79, 48, 10, 0 };
-		gridBagLayout.columnWeights = new double[] { 0.0, 0.0, 1.0, 0.0, Double.MIN_VALUE };
-		gridBagLayout.rowWeights = new double[] { 0.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 0.0, 0.0, Double.MIN_VALUE };
+		gridBagLayout.columnWidths = new int[]
+		{
+			25, 73, 200, 37, 0
+		};
+		gridBagLayout.rowHeights = new int[]
+		{
+			10, 57, 64, 53, 52, 55, 56, 79, 48, 10, 0
+		};
+		gridBagLayout.columnWeights = new double[]
+		{
+			0.0, 0.0, 1.0, 0.0, Double.MIN_VALUE
+		};
+		gridBagLayout.rowWeights = new double[]
+		{
+			0.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 0.0, 0.0, Double.MIN_VALUE
+		};
 		getContentPane().setLayout(gridBagLayout);
-
+		
 		JLabel lblTitre = new JLabel(VIEW_OPERATION_ALBUM_LABEL_TITLE);
 		lblTitre.setFont(new Font(FONT_USED, Font.PLAIN, 16));
 		GridBagConstraints gbc_lblTitre = new GridBagConstraints();
@@ -72,7 +85,7 @@ public class VuesOperationAlbum extends JDialog implements Constantes, Constante
 		gbc_lblTitre.gridx = 1;
 		gbc_lblTitre.gridy = 1;
 		getContentPane().add(lblTitre, gbc_lblTitre);
-
+		
 		textTitre = new JTextField();
 		GridBagConstraints gbc_textTitre = new GridBagConstraints();
 		gbc_textTitre.insets = new Insets(0, 0, 5, 5);
@@ -81,7 +94,7 @@ public class VuesOperationAlbum extends JDialog implements Constantes, Constante
 		gbc_textTitre.gridy = 1;
 		getContentPane().add(textTitre, gbc_textTitre);
 		textTitre.setColumns(10);
-
+		
 		JLabel lblPrix = new JLabel(VIEW_OPERATION_ALBUM_LABEL_PRICE);
 		lblPrix.setFont(new Font(FONT_USED, Font.PLAIN, 16));
 		GridBagConstraints gbc_lblPrix = new GridBagConstraints();
@@ -90,7 +103,7 @@ public class VuesOperationAlbum extends JDialog implements Constantes, Constante
 		gbc_lblPrix.gridx = 1;
 		gbc_lblPrix.gridy = 2;
 		getContentPane().add(lblPrix, gbc_lblPrix);
-
+		
 		textPrix = new JFormattedTextField(new Double(0));
 		textPrix.setColumns(10);
 		GridBagConstraints gbc_textPrix = new GridBagConstraints();
@@ -99,7 +112,7 @@ public class VuesOperationAlbum extends JDialog implements Constantes, Constante
 		gbc_textPrix.gridx = 2;
 		gbc_textPrix.gridy = 2;
 		getContentPane().add(textPrix, gbc_textPrix);
-
+		
 		JLabel lblGenre = new JLabel(VIEW_OPERATION_ALBUM_LABEL_GENRE);
 		lblGenre.setFont(new Font(FONT_USED, Font.PLAIN, 16));
 		GridBagConstraints gbc_lblGenre = new GridBagConstraints();
@@ -108,7 +121,7 @@ public class VuesOperationAlbum extends JDialog implements Constantes, Constante
 		gbc_lblGenre.gridx = 1;
 		gbc_lblGenre.gridy = 3;
 		getContentPane().add(lblGenre, gbc_lblGenre);
-
+		
 		textGenre = new JTextField();
 		textGenre.setColumns(10);
 		GridBagConstraints gbc_textGenre = new GridBagConstraints();
@@ -117,7 +130,7 @@ public class VuesOperationAlbum extends JDialog implements Constantes, Constante
 		gbc_textGenre.gridx = 2;
 		gbc_textGenre.gridy = 3;
 		getContentPane().add(textGenre, gbc_textGenre);
-
+		
 		JLabel lblAnneDeSortie = new JLabel(VIEW_OPERATION_ALBUM_LABEL_ANNEE);
 		lblAnneDeSortie.setFont(new Font(FONT_USED, Font.PLAIN, 16));
 		GridBagConstraints gbc_lblAnneDeSortie = new GridBagConstraints();
@@ -126,7 +139,7 @@ public class VuesOperationAlbum extends JDialog implements Constantes, Constante
 		gbc_lblAnneDeSortie.gridx = 1;
 		gbc_lblAnneDeSortie.gridy = 4;
 		getContentPane().add(lblAnneDeSortie, gbc_lblAnneDeSortie);
-
+		
 		datePicker = new DatePicker();
 		GridBagConstraints gbc_datePicker = new GridBagConstraints();
 		gbc_datePicker.fill = GridBagConstraints.HORIZONTAL;
@@ -134,16 +147,18 @@ public class VuesOperationAlbum extends JDialog implements Constantes, Constante
 		gbc_datePicker.gridx = 2;
 		gbc_datePicker.gridy = 4;
 		getContentPane().add(datePicker.getInterface(), gbc_datePicker);
-
-		JLabel lblMaisonDeDistribution = new JLabel(VIEW_OPERATION_ALBUM_LABEL_MAISON);
+		
+		JLabel lblMaisonDeDistribution = new JLabel(
+				VIEW_OPERATION_ALBUM_LABEL_MAISON);
 		lblMaisonDeDistribution.setFont(new Font(FONT_USED, Font.PLAIN, 16));
 		GridBagConstraints gbc_lblMaisonDeDistribution = new GridBagConstraints();
 		gbc_lblMaisonDeDistribution.anchor = GridBagConstraints.EAST;
 		gbc_lblMaisonDeDistribution.insets = new Insets(0, 0, 5, 5);
 		gbc_lblMaisonDeDistribution.gridx = 1;
 		gbc_lblMaisonDeDistribution.gridy = 5;
-		getContentPane().add(lblMaisonDeDistribution, gbc_lblMaisonDeDistribution);
-
+		getContentPane().add(lblMaisonDeDistribution,
+				gbc_lblMaisonDeDistribution);
+		
 		textMaison = new JTextField();
 		textMaison.setColumns(10);
 		GridBagConstraints gbc_textMaison = new GridBagConstraints();
@@ -152,7 +167,7 @@ public class VuesOperationAlbum extends JDialog implements Constantes, Constante
 		gbc_textMaison.gridx = 2;
 		gbc_textMaison.gridy = 5;
 		getContentPane().add(textMaison, gbc_textMaison);
-
+		
 		JLabel lblArtiste = new JLabel(VIEW_OPERATION_ALBUM_LABEL_ARTISTE);
 		lblArtiste.setFont(new Font(FONT_USED, Font.PLAIN, 16));
 		GridBagConstraints gbc_lblArtiste = new GridBagConstraints();
@@ -161,7 +176,7 @@ public class VuesOperationAlbum extends JDialog implements Constantes, Constante
 		gbc_lblArtiste.gridx = 1;
 		gbc_lblArtiste.gridy = 6;
 		getContentPane().add(lblArtiste, gbc_lblArtiste);
-
+		
 		comboBox = new JComboBox<Artiste>();
 		GridBagConstraints gbc_comboBox = new GridBagConstraints();
 		gbc_comboBox.insets = new Insets(0, 0, 5, 5);
@@ -169,13 +184,13 @@ public class VuesOperationAlbum extends JDialog implements Constantes, Constante
 		gbc_comboBox.gridx = 2;
 		gbc_comboBox.gridy = 6;
 		getContentPane().add(comboBox, gbc_comboBox);
-
-		if (typeOperation == RECHERCHER)
+		
+		if(typeOperation == RECHERCHER)
 			comboBox.addItem(null);
-		for (int i = 0; i < listArtistes.size(); i++) {
-			comboBox.addItem((Artiste) listArtistes.get(i));
+		for(int i = 0; i < listArtistes.size(); i++){
+			comboBox.addItem((Artiste)listArtistes.get(i));
 		}
-
+		
 		JButton btnChooseFile = new JButton("Choisir Image");
 		GridBagConstraints gbc_btnChooseFile = new GridBagConstraints();
 		gbc_btnChooseFile.fill = GridBagConstraints.HORIZONTAL;
@@ -183,7 +198,7 @@ public class VuesOperationAlbum extends JDialog implements Constantes, Constante
 		gbc_btnChooseFile.gridx = 1;
 		gbc_btnChooseFile.gridy = 7;
 		getContentPane().add(btnChooseFile, gbc_btnChooseFile);
-
+		
 		lblPath = new JLabel(VIEW_OPERATION_COMMON_LABEL_NO_PATH);
 		lblPath.setFont(new Font(FONT_USED, Font.PLAIN, 16));
 		GridBagConstraints gbc_lblPath = new GridBagConstraints();
@@ -192,7 +207,7 @@ public class VuesOperationAlbum extends JDialog implements Constantes, Constante
 		gbc_lblPath.gridx = 2;
 		gbc_lblPath.gridy = 7;
 		getContentPane().add(lblPath, gbc_lblPath);
-
+		
 		JButton btnConfirmer = new JButton();
 		GridBagConstraints gbc_btnConfirmer = new GridBagConstraints();
 		gbc_btnConfirmer.fill = GridBagConstraints.BOTH;
@@ -200,136 +215,171 @@ public class VuesOperationAlbum extends JDialog implements Constantes, Constante
 		gbc_btnConfirmer.gridx = 1;
 		gbc_btnConfirmer.gridy = 8;
 		getContentPane().add(btnConfirmer, gbc_btnConfirmer);
-
-		btnConfirmer.addActionListener(new ActionListener() {
-
+		
+		btnConfirmer.addActionListener(new ActionListener(){
+			
 			@Override
-			public void actionPerformed(ActionEvent e) {
-
+			public void actionPerformed(ActionEvent e){
+				
 				String titre = textTitre.getText();
-
+				
 				double prix = -1;
-				if (!textPrix.getText().isEmpty())
+				if(!textPrix.getText().isEmpty())
 					prix = Double.parseDouble(textPrix.getText());
-
+				
 				String genre = textGenre.getText();
-
+				
 				Date annee = null;
-
-				if (datePicker.isChanged())
+				
+				if(datePicker.isChanged())
 					annee = datePicker.getDate();
-
+				
 				String maison = textMaison.getText();
-
-				// TODO Copy image to source project and get filePath;
-
+				
 				String filePath = null;
-
-				Artiste artiste = (Artiste) comboBox.getSelectedItem();
-
-				try {
-
-					if (typeOperation != RECHERCHER) {
-
-						if (!titre.matches(REGEX_TITRE) && !genre.matches(REGEX_GENRE) && !maison.matches(REGEX_MAISON)) {
-
+				
+				String currentAlbumImage = lblPath.getText();
+				
+				if(!currentAlbumImage
+						.equals(VIEW_OPERATION_COMMON_LABEL_NO_PATH)){
+					
+					if(album == null){
+						
+						filePath = OutilsFichiers
+								.copyImageToProject(currentAlbumImage);
+						
+					}
+					else{
+						if(!currentAlbumImage.equals(album.getImagePath())){
+							filePath = OutilsFichiers
+									.copyImageToProject(currentAlbumImage);
+						}
+					}
+					
+				}
+				
+				Artiste artiste = (Artiste)comboBox.getSelectedItem();
+				
+				try{
+					
+					if(typeOperation != RECHERCHER){
+						
+						if(!titre.matches(REGEX_TITRE)
+								&& !genre.matches(REGEX_GENRE)
+								&& !maison.matches(REGEX_MAISON)){
+							
 							throw new Exception(ERROR_INVALID_FIELDS);
-
-						} else if (!titre.matches(REGEX_TITRE)) {
-
+							
+						}
+						else if(!titre.matches(REGEX_TITRE)){
+							
 							throw new Exception(ERROR_INVALID_TITRE);
-
-						} else if (!genre.matches(REGEX_GENRE)) {
-
+							
+						}
+						else if(!genre.matches(REGEX_GENRE)){
+							
 							throw new Exception(ERROR_INVALID_GENRE);
-
-						} else if(!maison.matches(REGEX_MAISON)){
+							
+						}
+						else if(!maison.matches(REGEX_MAISON)){
 							
 							throw new Exception(ERROR_INVALID_MAISON);
 							
-						}else if(!datePicker.isChanged()){
+						}
+						else if(!datePicker.isChanged()){
 							
 							throw new Exception(ERROR_INVALID_DATE);
 							
 						}
-
+						
 					}
-
-					switch (typeOperation) {
+					
+					switch(typeOperation){
 					case AJOUTER:
-
-						Album nouvAlbum = new Album(database, titre, prix, genre, annee, maison, filePath, artiste);
-
+						
+						Album nouvAlbum = new Album(database, titre, prix,
+								genre, annee, maison, filePath, artiste);
+						
 						vueAlbum.getTable().addItem(nouvAlbum);
-
+						
 						nouvAlbum.addToDatabase();
-
+						
 						break;
-
+					
 					case MODIFIER:
-
-						album.modifyItem(titre, prix, genre, annee, maison, filePath, artiste.getID());
-
+						
+						album.modifyItem(titre, prix, genre, annee, maison,
+								filePath, artiste.getID());
+						
 						break;
-
+					
 					case RECHERCHER:
-
+						
 						ArrayList<String> columnList = new ArrayList<>();
 						ArrayList<Object> valuesList = new ArrayList<>();
-
-						if (!titre.isEmpty()) {
-							columnList.add(Album.COLUMN_NAMES[Album.COLUMN_TITLE]);
+						
+						if(!titre.isEmpty()){
+							columnList
+									.add(Album.COLUMN_NAMES[Album.COLUMN_TITLE]);
 							valuesList.add(titre);
 						}
-
-						if (prix != -1) {
-							columnList.add(Album.COLUMN_NAMES[Album.COLUMN_PRICE]);
+						
+						if(prix != -1){
+							columnList
+									.add(Album.COLUMN_NAMES[Album.COLUMN_PRICE]);
 							valuesList.add(prix);
 						}
-
-						if (!genre.isEmpty()) {
-							columnList.add(Album.COLUMN_NAMES[Album.COLUMN_GENRE]);
+						
+						if(!genre.isEmpty()){
+							columnList
+									.add(Album.COLUMN_NAMES[Album.COLUMN_GENRE]);
 							valuesList.add(genre);
 						}
-
-						if (annee != null) {
-							columnList.add(Album.COLUMN_NAMES[Album.COLUMN_RELEASE_DATE]);
+						
+						if(annee != null){
+							columnList
+									.add(Album.COLUMN_NAMES[Album.COLUMN_RELEASE_DATE]);
 							valuesList.add(annee);
 						}
-
-						if (!maison.isEmpty()) {
-							columnList.add(Album.COLUMN_NAMES[Album.COLUMN_DISTRIBUTION]);
+						
+						if(!maison.isEmpty()){
+							columnList
+									.add(Album.COLUMN_NAMES[Album.COLUMN_DISTRIBUTION]);
 							valuesList.add(maison);
 						}
-
-						if (filePath != null) {
-							columnList.add(Album.COLUMN_NAMES[Album.COLUMN_IMAGE_URL]);
+						
+						if(filePath != null){
+							columnList
+									.add(Album.COLUMN_NAMES[Album.COLUMN_IMAGE_URL]);
 							valuesList.add(filePath);
 						}
-
-						if (artiste != null) {
-							columnList.add(Album.COLUMN_NAMES[Album.COLUMN_ARTIST]);
+						
+						if(artiste != null){
+							columnList
+									.add(Album.COLUMN_NAMES[Album.COLUMN_ARTIST]);
 							valuesList.add(artiste.getID());
 						}
-
-						columnNames = columnList.toArray(new String[columnList.size()]);
+						
+						columnNames = columnList.toArray(new String[columnList
+								.size()]);
 						values = valuesList.toArray();
-
+						
 						break;
 					}
-
+					
 					hasConfirmed = true;
-
+					
 					dispose();
-
-				} catch (Exception error) {
+					
+				}
+				catch(Exception error){
 					JOptionPane.showMessageDialog(null, error.getMessage());
 				}
-
+				
 			}
-
+			
 		});
-
+		
 		JButton btnAnnuler = new JButton(VIEW_OPERATION_COMMON_BOUTON_ANNULER);
 		GridBagConstraints gbc_btnAnnuler = new GridBagConstraints();
 		gbc_btnAnnuler.insets = new Insets(0, 0, 5, 5);
@@ -337,114 +387,121 @@ public class VuesOperationAlbum extends JDialog implements Constantes, Constante
 		gbc_btnAnnuler.gridx = 2;
 		gbc_btnAnnuler.gridy = 8;
 		getContentPane().add(btnAnnuler, gbc_btnAnnuler);
-
-		btnAnnuler.addActionListener(new ActionListener() {
-
+		
+		btnAnnuler.addActionListener(new ActionListener(){
+			
 			@Override
-			public void actionPerformed(ActionEvent e) {
-
+			public void actionPerformed(ActionEvent e){
+				
 				dispose();
-
+				
 			}
 		});
-
-		btnChooseFile.addActionListener(new ActionListener() {
-
+		
+		btnChooseFile.addActionListener(new ActionListener(){
+			
 			@Override
-			public void actionPerformed(ActionEvent e) {
-
+			public void actionPerformed(ActionEvent e){
+				
 				String filePath = null;
-
-				JFileChooser filechooser = new JFileChooser(
-						FileSystemView.getFileSystemView().getHomeDirectory().getAbsolutePath());
-				filechooser.addChoosableFileFilter(new FileNameExtensionFilter(PROJECT_IMAGES_FOLDER_NAME, POSSIBLE_EXTENSIONS));
+				
+				JFileChooser filechooser = new JFileChooser(FileSystemView
+						.getFileSystemView().getHomeDirectory()
+						.getAbsolutePath());
+				filechooser.addChoosableFileFilter(new FileNameExtensionFilter(
+						PROJECT_IMAGES_FOLDER_NAME, POSSIBLE_EXTENSIONS));
 				filechooser.setAcceptAllFileFilterUsed(false);
-
-				if (filechooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
-
+				
+				if(filechooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION){
+					
 					filePath = filechooser.getSelectedFile().getPath();
-
+					
 				}
-
-				try {
-
+				
+				try{
+					
 					File file = new File(filePath);
-
-					if (file.isFile()) {
+					
+					if(file.isFile()){
 						lblPath.setText(filePath);
-					} else {
+					}
+					else{
 						throw new Exception();
 					}
-
-				} catch (NullPointerException e1) {
-
-					lblPath.setText(VIEW_OPERATION_COMMON_LABEL_NO_PATH);
-
-				} catch (Exception e2) {
-
-					JOptionPane.showConfirmDialog(null, ERROR_FILE_NOT_EXIST, COMMON_ERROR, JOptionPane.DEFAULT_OPTION,
-							JOptionPane.ERROR_MESSAGE);
-
+					
 				}
-
+				catch(NullPointerException e1){
+					
+					lblPath.setText(VIEW_OPERATION_COMMON_LABEL_NO_PATH);
+					
+				}
+				catch(Exception e2){
+					
+					JOptionPane.showConfirmDialog(null, ERROR_FILE_NOT_EXIST,
+							COMMON_ERROR, JOptionPane.DEFAULT_OPTION,
+							JOptionPane.ERROR_MESSAGE);
+					
+				}
+				
 			}
 		});
-
-		switch (typeOperation) {
-
+		
+		switch(typeOperation){
+		
 		case AJOUTER:
 			btnConfirmer.setText(VIEW_OPERATIONS_AJOUTER);
 			setTitle(TITLE_AJOUT_ARTISTE + " - " + TITLE_PROJECT);
 			break;
-
+		
 		case MODIFIER:
 			btnConfirmer.setText(VIEW_OPERATIONS_MODIFIER);
 			setTitle(TITLE_MODIF_ARTISTE + " - " + TITLE_PROJECT);
 			setDefaultText(album);
 			break;
-
+		
 		case RECHERCHER:
 			btnConfirmer.setText(VIEW_OPERATIONS_RECHERCHER);
 			setTitle(TITLE_RECHERCHE_ARTISTE + " - " + TITLE_PROJECT);
 			break;
 		}
-
+		
 		setVisible(true);
-
+		
 	}
-
-	public void setDefaultText(Album album) {
-
+	
+	public void setDefaultText(Album album){
+		
 		textTitre.setText(album.getTitre());
-
+		
 		textPrix.setText(String.valueOf(album.getPrix()));
-
+		
 		textGenre.setText(album.getGenre());
-
+		
 		textMaison.setText(album.getMaisonDistribution());
-
+		
 		Calendar date = Calendar.getInstance();
-
+		
 		date.setTime(album.getAnneeSortie());
-
-		datePicker.setDate(date.get(Calendar.YEAR), date.get(Calendar.MONTH), date.get(Calendar.DAY_OF_MONTH));
-
+		
+		datePicker.setDate(date.get(Calendar.YEAR), date.get(Calendar.MONTH),
+				date.get(Calendar.DAY_OF_MONTH));
+		
 		comboBox.setSelectedItem(album.getArtiste());
-
+		
 		lblPath.setText(album.getImagePath());
-
+		
 	}
-
-	public boolean hasConfirmed() {
+	
+	public boolean hasConfirmed(){
 		return hasConfirmed;
 	}
-
-	public String[] getColumnNames() {
+	
+	public String[] getColumnNames(){
 		return columnNames;
 	}
-
-	public Object[] getValues() {
+	
+	public Object[] getValues(){
 		return values;
 	}
-
+	
 }
