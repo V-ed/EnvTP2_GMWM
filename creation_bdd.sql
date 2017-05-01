23c6818606d2e718735b63727c1cf405d217e7b0
whippet217 <whippet217@gmail.com>
2017-04-30 21:49:20 -0400

    Ajout d'artistes et d'albums de base à la base de donné

--- creation_bdd.sql
+++ creation_bdd.sql
@@ -19,5 +19,17 @@ CREATE TABLE album (
 	maisonDistribution VARCHAR(40) NOT NULL,
 	image VARCHAR(30),
 	idArtiste int,
-	FOREIGN KEY (idArtiste) REFERENCES artiste(idArtiste) ON DELETE CASCADE
+	FOREIGN KEY (idArtiste) REFERENCES artiste(idArtiste)
 );
+
+INSERT INTO artiste (nom, prenom, estMembre, image) VALUES ('Cooper', 'John', true, null);
+INSERT INTO artiste (nom, prenom, estMembre, image) VALUES ('Peters', 'Lori', true, null);
+INSERT INTO artiste (nom, prenom, estMembre, image) VALUES ('Ledger', 'Jen', false, null);
+INSERT INTO artiste (nom, prenom, estMembre, image) VALUES ('Cooper', 'Korey', false, null);
+INSERT INTO artiste (nom, prenom, estMembre, image) VALUES ('Morrison', 'Seth', false, null);
+
+INSERT INTO album (titre, prix, genre, anneeSortie, maisonDistribution, image, idArtiste) Values ('Rise', 50.99, 'Rock', '2013-06-25', 'CPM Distribution', null, 1);
+INSERT INTO album (titre, prix, genre, anneeSortie, maisonDistribution, image, idArtiste) Values ('Collide', 25, 'Classique', '2003-11-18', 'Tonality', null, 2);
+INSERT INTO album (titre, prix, genre, anneeSortie, maisonDistribution, image, idArtiste) Values ('Alien Youth', 50.99, '', '2001-08-28', 'TRX Distribution', null, 3);
+INSERT INTO album (titre, prix, genre, anneeSortie, maisonDistribution, image, idArtiste) Values ('Awake', 45.99, 'Rock', '2009-08-25', 'Howard Benson', null, 4);
+INSERT INTO album (titre, prix, genre, anneeSortie, maisonDistribution, image, idArtiste) Values ('Comatose', 70.99, 'Rock', '2006-10-03', 'Howard Benson', null, 4);
