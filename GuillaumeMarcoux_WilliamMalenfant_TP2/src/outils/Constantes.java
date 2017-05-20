a2dbf7bc8565e15f88865cf6eda07156b4fd5b42
V-ed <guillaumemarcoux@gmail.com>
2017-05-20 11:08:40 -0400

    ok

--- GuillaumeMarcoux_WilliamMalenfant_TP2/src/outils/Constantes.java
+++ GuillaumeMarcoux_WilliamMalenfant_TP2/src/outils/Constantes.java
@@ -26,10 +26,10 @@ public interface Constantes {
 	
 	final String LABEL_NAME = "artiste";
 	
-	final String IMAGE_ARTISTE_MOUSE_ON = "/images/artiste_mouse_on.jpg";
-	final String IMAGE_ALBUM_MOUSE_ON = "/images/album_mouse_on.jpg";
-	final String IMAGE_ARTISTE_MOUSE_OUT = "/images/artiste_mouse_out.jpg";
-	final String IMAGE_ALBUM_MOUSE_OUT = "/images/album_mouse_out.jpg";
+	final String IMAGE_ARTISTE_MOUSE_ON = "artiste_mouse_on.jpg";
+	final String IMAGE_ALBUM_MOUSE_ON = "album_mouse_on.jpg";
+	final String IMAGE_ARTISTE_MOUSE_OUT = "artiste_mouse_out.jpg";
+	final String IMAGE_ALBUM_MOUSE_OUT = "album_mouse_out.jpg";
 	final String IMAGE_CONNECT_USER = "utilisateur.png";
 	final String IMAGE_CONNECT_KEY = "cle.png";
 	final String IMAGE_CONNECT_CHECK = "crochet_1.png";
